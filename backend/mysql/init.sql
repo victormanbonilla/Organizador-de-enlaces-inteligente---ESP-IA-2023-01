28dc428da56c68fd346811c5b4036e70efc4d809
victorbonilla-sw <73923484+victormanbonilla@users.noreply.github.com>
2023-03-13 13:03:12 -0500

    Update init.sql

--- backend/mysql/init.sql
+++ backend/mysql/init.sql
@@ -7,12 +7,14 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`)
 );
 
+ALTER TABLE users ADD INDEX id_google_index (id_google);
+
 CREATE TABLE `lists` (
   `id` INT AUTO_INCREMENT,
   `code` VARCHAR(255) NOT NULL,
   `created_at` VARCHAR(255) NOT NULL,
   `user_id_google` VARCHAR(255) NOT NULL,
-  `data` VARCHAR(2048) NOT NULL,
+  `data` VARCHAR(65535) NOT NULL,
   PRIMARY KEY (`id`),
   FOREIGN KEY (`user_id_google`) REFERENCES users (`id_google`)
 );
